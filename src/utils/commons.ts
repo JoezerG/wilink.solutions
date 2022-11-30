b65fd7e02d048ef6130d81f78f1e32e731d103fb
JoezerG <joezergullo@gmail.com>
2022-11-29 19:02:22 -0500

    create date utils

---
+++ src/utils/commons.ts
@@ -0,0 +1,25 @@
+import { MarkdownInstance } from "astro";
+import { DateTime } from "luxon";
+import type { PostMetada } from "../utils/interfaces";
+
+export const sortPostByDate = (
+  current: MarkdownInstance<PostMetada>,
+  prev: MarkdownInstance<PostMetada>
+) => {
+  const dateA = DateTime.fromFormat(
+    current.frontmatter.date,
+    "yyyy-MM-dd"
+  ).toMillis();
+  const dateB = DateTime.fromFormat(
+    prev.frontmatter.date,
+    "yyyy-MM-dd"
+  ).toMillis();
+
+  return dateB - dateA;
+};
+
+export const formatDate = (date: string) => {
+  return DateTime.fromFormat(date, "yyyy-MM-dd").toLocaleString(
+    DateTime.DATE_MED_WITH_WEEKDAY
+  );
+};
