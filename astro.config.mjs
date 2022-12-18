f11632b446de006cec9b3bde1c574bd917cdce49
JoezerG <joezergullo@gmail.com>
2022-12-17 22:05:23 -0500

    config sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
 export default defineConfig({
+  site: "https://wilink.solutions",
   integrations: [sitemap(), partytown()],
 });
