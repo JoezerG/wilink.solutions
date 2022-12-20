cf311d8a27d3a25ca8e0590e9ba13ad04c64d6b9
JoezerG <joezergullo@gmail.com>
2022-12-20 14:18:25 -0500

    remove luxon

--- src/utils/commons.ts
+++ src/utils/commons.ts
@@ -1,28 +1,17 @@
 import { MarkdownInstance } from "astro";
-import { DateTime } from "luxon";
 import type { PostMetada } from "../utils/interfaces";
 
 export const sortPostByDate = (
   current: MarkdownInstance<PostMetada>,
   prev: MarkdownInstance<PostMetada>
 ) => {
-  const dateA = DateTime.fromFormat(
-    current.frontmatter.date,
-    "yyyy-MM-dd"
-  ).toMillis();
-  const dateB = DateTime.fromFormat(
-    prev.frontmatter.date,
-    "yyyy-MM-dd"
-  ).toMillis();
+  const dateA = new Date(current.frontmatter.date).getTime();
+  const dateB = new Date(prev.frontmatter.date).getTime();
 
   return dateB - dateA;
 };
 
 export const formatDate = (date: string) => {
-  return DateTime.fromFormat(date, "yyyy-MM-dd").toLocaleString(
-    DateTime.DATE_MED_WITH_WEEKDAY,
-    {
-      locale: "es",
-    }
-  );
+  const _date = new Date(date);
+  return new Intl.DateTimeFormat("es", { dateStyle: "medium" }).format(_date);
 };
